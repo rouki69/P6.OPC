ff7e44d289967f59c89e3ed8db18dcd718129a25
rouki69 <69466815+rouki69@users.noreply.github.com>
2021-02-16 01:47:21 +0100

    Update script-python.py

--- script-python.py
+++ script-python.py
@@ -1,186 +1,236 @@
-    # Importer tous les objets important
-    import getpass
-    import os
-    import sys
-    from csv import reader
-    from datetime import date, datetime
-    from netmiko import ConnectHandler
-    
-    
-    # Verifier si le dossier existe sinon le créer(pour les backups).
-    if not os.path.exists('Backup-Configs'):
-        os.makedirs('Backup-Configs')
-    
-    # Format de la date et de l'heure
-    now = datetime.now()
-    dt_string = now.strftime("%d-%m-%Y_%H-%M")
-    
-    def banner():
-        welcome = """\033[92m
-    ======================================================
-    ======================================================
-    ============BIENVENUE PROJET 6 MR REMI================
-    =============SCRIPT PYTHON POUR CISCO=================
-    ===============POUR OPENCLASSROOM=====================	
-    ======================================================
-    ======================================================
-    
-     \033[0m"""
-        return welcome
-    
-    # Les informations necessaire pour se connecter
-    def get_saved_config(host, username, password, enable_secret):
-        cisco_ios = {
-            'device_type': 'cisco_ios',
-            'host': host,
-            'username': username,
-            'password': password,
-            'secret': enable_secret,
-        }
-        # Connexion avec l'appareil
-        net_connect = ConnectHandler(**cisco_ios)
+# Importer tous les objets important
+import getpass
+import os
+import sys
+from csv import reader
+from datetime import date, datetime
+from netmiko import ConnectHandler
+
+# Verifier si le dossier existe sinon le créer(pour les backups).
+if not os.path.exists('Backup-Configs'):
+    os.makedirs('Backup-Configs')
+
+# Format de la date et de l'heure
+now = datetime.now()
+dt_string = now.strftime("%d-%m-%Y_%H-%M")
+
+
+def banner():
+    welcome = """\033[92m
+======================================================
+======================================================
+======================================================
+========== SCRIPT PYTHON POUR CISCO===================
+======================================================	
+======================================================
+======================================================
+
+ \033[0m"""
+    return welcome
+
+
+# Initialiser la connection
+def get_connection():
+    device = {
+        'device_type': 'cisco_ios',
+        'host': input("\nHost: "),
+        'username': input("Utilisateur: "),
+        'password': getpass.getpass("Mot de passe: "),
+        'secret': getpass.getpass("Mot de passe Secret: "),
+    }
+    try:
+        net_connect = ConnectHandler(**device)
         net_connect.enable()
-        # Recuperation de la configuration actuelle
-        output = net_connect.send_command("show running-config")
-        #Recuperation du nom de l'appareil.
-        hostname = net_connect.send_command("show run | in hostname")
-        hostname = hostname.split()
-        hostname = hostname[1]
-        # Creation du fichier avec le nom la date et l'heure.
-        fileName = hostname + "_" + dt_string
-        # Creation du fichier texte dans le dossier backup config avec le nom la date et l'heure.
-        backupFile = open("Backup-Configs/" + fileName + ".txt", "w+")
-        backupFile.write(output)
-        print("Backup creer dans " + fileName + ".txt!")
-    
-    #Permettre le changement de la passerelle par défaut    
-    def get_gateway(host, username, password, enable_secret):    
-        cisco_ios = {
-            'device_type': 'cisco_ios',
-            'host': host,
-            'username': username,
-            'password': password,
-            'secret': enable_secret,
-        }
-        # Connexion avec l'appareil
-        net_connect = ConnectHandler(**cisco_ios)
+        print(f"\nVous êtes bien connecté à {device['host']}")
+        print("-------------------------------------------------\n")
+        return net_connect  # On retourne la connexion.
+    except:
+        # Message d'erreur si la connection est impossible avec l'appareil
+        print(f"\nImpossible de se connecter à {device['host']}. Vérifier que les identifiants saisis sont valides.")
+        exit()
+
+
+# Les informations necessaire pour se connecter
+def get_manuel_config():
+    net_connect = get_connection()
+    # Recuperation de la configuration actuelle
+    output = net_connect.send_command("show running-config")
+    # Recuperation du nom de l'appareil.
+    hostname = net_connect.send_command("show run | in hostname")
+    hostname = hostname.split()
+    hostname = hostname[1]
+    # Creation du fichier avec le nom la date et l'heure.
+    fileName = hostname + "_" + dt_string
+    # Creation du fichier texte dans le dossier backup config avec le nom la date et l'heure.
+    backupFile = open("Backup-Configs/" + fileName + ".txt", "w+")
+    backupFile.write(output)
+    print("\nBackup creer dans " + fileName + ".txt!")
+    net_connect.disconnect()
+
+
+# Permet de faire le backup en prenant les informations directement du fichier CSV.
+def get_saved_config(host, username, password, enable_secret):
+    device = {
+        'device_type': 'cisco_ios',
+        'host': host,
+        'username': username,
+        'password': password,
+        'secret': enable_secret,
+    }
+    # Connexion avec l'appareil
+    try:
+        net_connect = ConnectHandler(**device)
         net_connect.enable()
-        print('\nEntrer la nouvelle passerelle :')
-        new_gateway = input()
-        result = net_connect.send_config_set("ip default-gateway {0} ".format(new_gateway))
-        print(result)
-        
-        
-    # Demander l'adresse ip, le nom d'utilisateur, le mot de passe et le mot de passe secret.
-    def manual_option():
-        host = input("\nIP: ")
-        username = input("Utilisateur: ")
-        password = getpass.getpass("Mot de Passe: ")
-        enable_secret = getpass.getpass("Mot de passe secret: ")
-        get_saved_config(host, username, password, enable_secret)
-    
-    # Recuper le fichier CSV et recuperer les informations dedans.
-    def csv_option():
-        csv_name = input("\nQuel est le nom du fichier CSV: ")
-        with open(csv_name, 'r') as read_obj:
-            csv_reader = reader(read_obj)
-            list_of_rows = list(csv_reader)
-            rows = len(list_of_rows)
-            while rows >= 2:
-                rows = rows - 1
-                get_saved_config(list_of_rows[rows][0], list_of_rows[rows][1], list_of_rows[rows][2], list_of_rows[rows][3])
-    
-    #Affichage de la table de routage des routeurs
-    def change_gateway():
-        host = input("\nIP: ")
-        username = input("Utilisateur ")
-        password = getpass.getpass("Mot de passe: ")
-        enable_secret = getpass.getpass("Mot de passe secret: ")
-        get_gateway(host, username, password, enable_secret)
-        
-    #Faire un show route
-    def get_show_route(host, username, password, enable_secret):    
-        cisco_ios = {
-            'device_type': 'cisco_ios',
-            'host': host,
-            'username': username,
-            'password': password,
-            'secret': enable_secret,
-        }
-        # Connexion avec l'appareil
-        net_connect = ConnectHandler(**cisco_ios)
-        net_connect.enable()
-        result = net_connect.send_command("show ip route")
-        print(result)
-    
-        
-    #Recuperer information pour show route
-    def show_route():
-        host = input("\nIP: ")
-        username = input("Utilisateur ")
-        password = getpass.getpass("Mot de passe: ")
-        enable_secret = getpass.getpass("Mot de passe secret: ")
-        get_show_route(host, username, password, enable_secret)
-        
-        
-    #Connaitre l'adresse ip des interfaces
-    def get_int_ip(host, username, password, enable_secret):    
-        cisco_ios = {
-            'device_type': 'cisco_ios',
-            'host': host,
-            'username': username,
-            'password': password,
-            'secret': enable_secret,
-        }
-        # Connexion avec l'appareil
-        net_connect = ConnectHandler(**cisco_ios)
-        net_connect.enable()
-        result = net_connect.send_command("show ip interface brief")
-        print(result)
-    
-        
-    #Donnee pour interface IP
-    def show_int_ip():
-        host = input("\nIP: ")
-        username = input("Utilisateur ")
-        password = getpass.getpass("Mot de passe: ")
-        enable_secret = getpass.getpass("Mot de passe secret: ")
-        get_int_ip(host, username, password, enable_secret)    
-       
-        
-        
-    print (banner() + """\033[96m """)
-    # Demander a l utilisateur les options qu ils souhaitent
-    print("\n1. Backup en rentrant vous même les IP.")
-    print("2. Backup en recuperant les informations par le fichier CSV.")
-    print("3. Changer la passerelle d'un routeur.")
-    print("4. Faire un show route d'un routeur")
-    print("5. Récuperer les IP des interfaces.")
-    print("0. Quitter le script.")
-    
-    choice = input("\nMerci de choisir une option: ")
-    # Pour lancer les differentes options.
-    
-    if choice == "1":
-        #Combien d'appareil l'utilisateur veut selectionner.
-        how_many = input("Combien d'appareil voulez vous selectionner: ")
-        how_many = int(how_many)
-        i = how_many
-        while i >= 1:
-            manual_option()
-            i = i - 1
-    elif choice == "2":
-        # Utiliser l'option CSV.
-         csv_option()
-    elif choice == "3":
-    #Choix pour changer la passerelle
-         change_gateway()  
-    elif choice == "4":
-         show_route() 
-    #Choix pour faire un show route
-    elif choice == "5":
-         show_int_ip()
-    elif choice == "0":
-         sys.exit("\nMerci d'avoir utilisé le script au revoir\n")
-    else :
-         sys.exit("\n --Merci de séléctionner une option val
+        print(f"\nVous êtes bien connecté à {device['host']}")
+        # Continue le code si la connection est établie
+        print("-------------------------------------------------\n") \
+            # Message d'erreur si la connection est impossible avec l'appareil
+    except:
+        print(f"\nImpossible de se connecter à {device['host']}. Vérifier que les identifiants saisis sont valides.")
+        exit()
+    # Recuperation de la configuration actuelle
+    output = net_connect.send_command("show running-config")
+    # Recuperation du nom de l'appareil.
+    hostname = net_connect.send_command("show run | in hostname")
+    hostname = hostname.split()
+    hostname = hostname[1]
+    # Creation du fichier avec le nom la date et l'heure.
+    fileName = hostname + "_" + dt_string
+    # Creation du fichier texte dans le dossier backup config avec le nom la date et l'heure.
+    backupFile = open("Backup-Configs/" + fileName + ".txt", "w+")
+    backupFile.write(output)
+    print("Backup creer dans " + fileName + ".txt!")
+    net_connect.disconnect()
+
+
+# Recuper le fichier CSV et recuperer les informations dedans.
+def csv_option():
+    csv_name = input("\nQuel est le nom du fichier CSV: ")
+    with open(csv_name, 'r') as read_obj:
+        csv_reader = reader(read_obj)
+        list_of_rows = list(csv_reader)
+        rows = len(list_of_rows)
+        while rows >= 2:
+            rows = rows - 1
+            get_saved_config(list_of_rows[rows][0], list_of_rows[rows][1], list_of_rows[rows][2], list_of_rows[rows][3])
+
+
+# Changer la passerelle par défaut
+def change_gateway():
+    net_connect = get_connection()
+    print('Entrer la nouvelle passerelle :')
+    new_gateway = input()
+    net_connect.send_config_set("ip default-gateway {0} ".format(new_gateway))
+    result = net_connect.send_command("show ip default-gateway")
+    print(f"La nouvelle passerelle par défaut est: {result}")
+    net_connect.send_command("wr")
+    net_connect.disconnect()
+
+
+# Faire un show route
+def show_route():
+    net_connect = get_connection()
+    result = net_connect.send_command("show ip route")
+    print(f"\n{result}")
+    net_connect.disconnect()
+
+
+# Montrer les ip associés aux interfaces
+def show_int_ip():
+    net_connect = get_connection()
+    result = net_connect.send_command("show ip interface brief")
+    print(f"\n{result}")
+    net_connect.disconnect()
+
+
+# Montrer la version d'IOS
+def get_version():
+    net_connect = get_connection()
+    version = net_connect.send_command("show version | in IOS")
+    print(f"\n{version}")
+    net_connect.disconnect()
+
+
+# Montrer la table ARP
+def show_arp():
+    net_connect = get_connection()
+    output = net_connect.send_command("show arp")
+    print(f"\n{output}")
+    net_connect.disconnect()
+
+
+# Ajouter un utilisateur
+def add_user():
+    net_connect = get_connection()
+    result = net_connect.send_command("show run | inc username ")
+    username = input("\nChoisissez un nom d'utilisateur: ")
+    if username in result:
+        print("\nL'utilisateur existe déjà veuillez réessayer")
+        exit()
+    else:
+        password = getpass.getpass("Choisissez un mot de passe: ")
+        print(f"\nCréation de l'utilisateur {username}...")
+    net_connect.config_mode()
+    result = net_connect.send_command(f"username {username} password {password}")
+    print(f"\n{username} à bien été créé\n")
+    net_connect.exit_config_mode()
+    result = net_connect.send_command("show run | inc username ")
+    print(result)
+    net_connect.send_command("wr")
+    net_connect.disconnect()
+
+
+def chance_enable_password():
+    net_connect = get_connection()
+    net_connect.config_mode()
+    password = getpass.getpass("\nChoisissez un nouveau mot de passe secret: ")
+    result = net_connect.send_command(f"enable secret {password}")
+    print("\nLe nouveau mot de passe secret est ajouté")
+    net_connect.send_command("wr")
+    net_connect.disconnect()
+
+
+print(banner() + """\033[96m """)
+# Demander a l utilisateur les options qu ils souhaitent
+print("\n1. Backup en rentrant vous même les IP.")
+print("2. Backup en recuperant les informations par le fichier CSV.")
+print("3. Changer la passerelle d'un routeur.")
+print("4. Faire un show route d'un routeur")
+print("5. Récuperer les IP des interfaces.")
+print("6. Connaitre la version d'IOS.")
+print("7. Voir la table ARP.")
+print("8. Ajouter un utilisateur")
+print("9. Changer le mot de passe secret")
+print("0. Quitter le script.")
+
+# Pour lancer les differentes options.
+choice = input("\nMerci de choisir une option: ")
+
+if choice == "1":
+    # Combien d'appareil l'utilisateur veut selectionner.
+    how_many = input("\nCombien d'appareil voulez vous selectionner: ")
+    how_many = int(how_many)
+    i = how_many
+    while i >= 1:
+        get_manuel_config()
+        i = i - 1
+elif choice == "2":
+    csv_option()
+elif choice == "3":
+    change_gateway()
+elif choice == "4":
+    show_route()
+elif choice == "5":
+    show_int_ip()
+elif choice == "6":
+    get_version()
+elif choice == "7":
+    show_arp()
+elif choice == "8":
+    add_user()
+elif choice == "9":
+    chance_enable_password()
+elif choice == "0":
+    sys.exit("\nMerci d'avoir utilisé le script au revoir\n")
+else:
+    sys.exit("\n --Merci de séléctionner une option valide--\n")
